30ecd803e89f97094a6ce03990e498e03ecb40f6
aslan <social.smur@simplelogin.fr>
2020-03-28 19:17:41 +0000

    Completed

---
+++ etch-a-sketch/esketch.js
@@ -0,0 +1,49 @@
+
+        const container = document.querySelector('#container'); 
+
+
+        function defaultGrid(tiles){
+        for (let i=0; i < tiles*tiles; i++ ){
+                const div = document.createElement("div");
+                div.className = "squares";
+                container.appendChild(div)}
+                const squ = Array.from(document.querySelectorAll('.squares'));
+                squ.forEach(squ => squ.addEventListener("mouseover", mouseover));
+        }
+
+        function removeGrid(){
+                document.querySelectorAll('.squares').forEach(element => element.remove());
+        }
+
+        function mouseover(e){
+               this.style.backgroundColor = randomColor();
+                
+        }
+
+        function randomColor(){
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)]};
+                return color;
+        }
+
+        function reset(){
+                const squ = Array.from(document.querySelectorAll('.squares'));
+                squ.forEach(squ => squ.style.backgroundColor = "white");
+        }
+
+
+        function promptTiles(){
+                removeGrid();
+                let tiles = prompt("Enter number of tiles for grid");
+                container.style.gridTemplateRows = `repeat(${tiles},1fr)`;
+                container.style.gridTemplateColumns = `repeat(${tiles},1fr)`;
+                defaultGrid(tiles);
+                
+        }
+
+        defaultGrid(16)
+        
+        
+
