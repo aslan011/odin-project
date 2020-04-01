e6de82950cfe1362c3028202ff6c7f4cc6578986
aslan <social.smur@simplelogin.fr>
2020-04-01 16:50:32 +0100

    Added calc gui and operator function

--- calculator/calc.js
+++ calculator/calc.js
@@ -14,3 +14,24 @@ function divide (a, b) {
 function multiply (a, b){
     return a * b
 }
+
+function operator (a, b, op){
+	if (op == "+"){
+		result = add(a,b)
+	}
+	else if (op == "-"){
+		result = subtract(a,b)
+	}
+	else if (op == "/"){
+		result = divide(a,b)
+	}
+	else if (op == "*"){
+		result = multiply(a,b)
+	}
+	else {
+		result = "Error"
+	}
+	console.log(result)
+}
+
+operator(2,3,"")
