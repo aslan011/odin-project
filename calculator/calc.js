2d30c59f65f092a4d29eb5b0aff757c40ab25261
aslan <social.smur@simplelogin.fr>
2020-04-02 20:50:29 +0100

    Completed calculatorgit status!

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,5 +1,11 @@
+currentNumber = null 
+currentOp = null
+nextNumber = null
+operatorPressed = "N"
+result = null
+
 function add (a,b) {
-	return a+b
+	return a + b 
 	
 }
 function subtract (a,b) {
@@ -7,9 +13,11 @@ function subtract (a,b) {
 	
 }
 function divide (a, b) {
-	return a / b
-	
-}
+	if (b == 0){
+		alert("Nice try!")
+		clearAll();}
+	else {return a / b}
+	}
 
 function multiply (a, b){
     return a * b
@@ -29,27 +37,90 @@ function operator (a, b, op){
 		result = multiply(a,b)
 	}
 	else {
-		result = "Error"
+		return;
 	}
-	console.log(result)
+	
 }
 
 function numberSelect(n){
 	let button = document.getElementById("display");
-	button.textContent += n;
-	
+	if (operatorPressed == "Y"){
+
+		if (nextNumber == null){
+			button.textContent = n;
+			nextNumber = n
+		}
+		else {
+			button.textContent += n;
+			nextNumber += n
+		}
+	}
+
+	else {
+		if (currentNumber == null){
+		button.textContent = n;
+		currentNumber = n	
+	}
+		else {
+			button.textContent += n;
+			currentNumber += n
+		}
+	}
 }
 
 function operatorSelect(n){
-	let operator = document.getElementById("display");
-	operator.textContent += ` ${n} `;
+	if (currentNumber == null){
+		return;
+	}
+	else{
+	operatorPressed = "Y"
+	currentOp = n;
+	}
+	
+}	
+
+function decimal(){
+	let button = document.getElementById("display");
+	if (operatorPressed == "Y" && !nextNumber.includes(".")){
+		nextNumber += "."
+		button.textContent = nextNumber;
+	}
+	else if (operatorPressed == "N" && !currentNumber.includes(".")) {
+		currentNumber += "."
+		button.textContent = currentNumber;
+	}
+
+	else {return}
 }
 
 function equals(){
-	let calculation = document.getElementById("display");
-	let currentCalc = calculation.textContent.split(" ");
-	calculation.textContent += " =";
-	console.log(currentCalc);
+	let button = document.getElementById("display");
+	if (button = ""){
+		return;
+	}
+
+	else if (nextNumber == null){
+		return;
+	}
+	else {
+	cNum = Number(currentNumber);
+	nNum = Number(nextNumber);
+	operator(cNum,nNum,currentOp);
+	let button = document.getElementById("display");
+	button.textContent = result;
+	currentNumber = result
+	nextNumber = null;
+	}
 
 }
 
+function clearAll(){
+	currentNumber = null 
+	currentOp = null
+	nextNumber = null
+	operatorPressed = "N"
+	result = null
+	let button = document.getElementById("display");
+	button.textContent = "";
+
+}
