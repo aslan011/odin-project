a6e989cc42eb1bee9f58b52e5bac11a84db336d6
aslan <social.smur@simplelogin.fr>
2020-05-05 17:56:21 +0100

    Cleaned with eslint

--- library/book.js
+++ library/book.js
@@ -1,134 +1,124 @@
 const container = document.querySelector('#container');
-let myLibrary = []
+const myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-function addBookToLibrary(title, author, pages, read){
-        //creates object from inputted data and appends it to library array
-        title = new Book(title, author, pages, read)
-        myLibrary.push(title);
-        render()
-        
+function addBookToLibrary(title, author, pages, read) {
+  // creates object from inputted data and appends it to library array
+  title = new Book(title, author, pages, read);
+  myLibrary.push(title);
+  render();
 }
 
-function toggleRead(index, e){
-    if (myLibrary[index]["read"] == "Not read"){
-        myLibrary[index]["read"] = "Read"
-        e.innerHTML = "Read"
-    }
-    else {
-        myLibrary[index]["read"] = "Not read"
-        e.innerHTML = "Read?"
-    }
+function toggleRead(index, e) {
+  if (myLibrary[index].read == 'Not read') {
+    myLibrary[index].read = 'Read';
+    e.innerHTML = 'Read';
+  } else {
+    myLibrary[index].read = 'Not read';
+    e.innerHTML = 'Read?';
+  }
 }
 
 function render() {
-    const div = document.createElement("div")
-    const button = document.createElement("button")
-    const read = document.createElement("button")
-    myLibrary.forEach(function(item){
-    div.className = "books";
-    button.className = "removeButton"
-    button.textContent = "Remove"
-    read.className = "readToggle"
-    if (item.read == "Not read"){
-    read.textContent = "Read?"
-    }
-    else {read.textContent = "Read"}
-    div.dataset.index = (myLibrary.length -1);
-    div.innerHTML = 
-            `<h1>${item.title}<h1> 
+  const div = document.createElement('div');
+  const button = document.createElement('button');
+  const read = document.createElement('button');
+  myLibrary.forEach((item) => {
+    div.className = 'books';
+    button.className = 'removeButton';
+    button.textContent = 'Remove';
+    read.className = 'readToggle';
+    if (item.read == 'Not read') {
+      read.textContent = 'Read?';
+    } else { read.textContent = 'Read'; }
+    div.dataset.index = (myLibrary.length - 1);
+    div.innerHTML = `<h1>${item.title}<h1> 
             <h2>${item.author}<h2>
-            <h3>${item.pages} pages<h3>`
-    container.appendChild(div)
-    div.appendChild(button)
-    div.appendChild(read)}
-    
-)}
+            <h3>${item.pages} pages<h3>`;
+    container.appendChild(div);
+    div.appendChild(button);
+    div.appendChild(read);
+  });
+}
 
-function removeBook (index){
-    myLibrary.splice(index,1);
-    const div = Array.from(document.getElementsByClassName("books"))
-    div.map(function(item){
-        if (item.dataset.index === index) {
-            container.removeChild(item)
-        }})
-    div.forEach(function(item){
-        if (item.dataset.index > index) {
-            item.dataset.index -= 1
-        }})
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  const div = Array.from(document.getElementsByClassName('books'));
+  div.map((item) => {
+    if (item.dataset.index === index) {
+      container.removeChild(item);
+    }
+  });
+  div.forEach((item) => {
+    if (item.dataset.index > index) {
+      item.dataset.index -= 1;
+    }
+  });
 }
 
 function newBook() {
-    document.getElementById("form").style.display = "block";
-    document.getElementById("addBook").style.display = "none";
+  document.getElementById('form').style.display = 'block';
+  document.getElementById('addBook').style.display = 'none';
 }
 
 function addBook() {
-    const form = document.getElementById("form");
-    if (form.checkValidity()){
-    const title = document.getElementById("title").value
-    const author = document.getElementById("author").value
-    const page = document.getElementById("pages").value
-    if (document.getElementById("read").checked == true){
-        read = "Read"}
-    else {read = "Not read"}
-    addBookToLibrary(title, author, page, read)
-    document.getElementById("form").style.display = "none";
-    document.getElementById("addBook").style.display = "block"; 
-    clearFields()
-}
-    else if (!title.checkValidity()){
-        document.getElementById("title").placeholder = "Please enter a title";
-    }
-
-    else if (!author.checkValidity()){
-        document.getElementById("author").placeholder = "Please enter an author name";
-    }
-
-    else {
-        document.getElementById("pages").placeholder = "Please enter number of pages";
-        document.getElementById("pages").value = "";
-    }
+  const form = document.getElementById('form');
+  if (form.checkValidity()) {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const page = document.getElementById('pages').value;
+    if (document.getElementById('read').checked == true) {
+      read = 'Read';
+    } else { read = 'Not read'; }
+    addBookToLibrary(title, author, page, read);
+    document.getElementById('form').style.display = 'none';
+    document.getElementById('addBook').style.display = 'block';
+    clearFields();
+  } else if (!title.checkValidity()) {
+    document.getElementById('title').placeholder = 'Please enter a title';
+  } else if (!author.checkValidity()) {
+    document.getElementById('author').placeholder = 'Please enter an author name';
+  } else {
+    document.getElementById('pages').placeholder = 'Please enter number of pages';
+    document.getElementById('pages').value = '';
+  }
 }
 
-function clearFields(){
-    document.getElementById("title").value = ""
-    document.getElementById("author").value = ""
-    document.getElementById("pages").value = ""
-    
+function clearFields() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('pages').value = '';
 }
 
 clearFields();
 
-addBookToLibrary("Open veins of South America", "Some guy", "456 pages")
-addBookToLibrary("Open veins of South America", "Some guy", "456 pages")
-addBookToLibrary("Open veins of South America", "Some guy", "456 pages")
-addBookToLibrary("PRisoners of Georgrpahy", "Some guy", "456 pages")
-addBookToLibrary("Open veins of South America", "Some guy", "456 pages")
-addBookToLibrary("Open veins of South America", "Some guy", "456 pages")
-addBookToLibrary("Open veins of South America", "Some guy", "456 pages")
-addBookToLibrary("Oh dearrrrrr", "Some guy", "456 pages")
-addBookToLibrary("58573739", "Some guy", "456 pages")
-addBookToLibrary("48 laws of power", "Some guy", "456 pages")
-addBookToLibrary("The subtle art of not", "Some guy", "456 pages")
-addBookToLibrary("How to run away from People", "Some guy", "456 pages")
-render()
+addBookToLibrary('Open veins of South America', 'Some guy', '456 pages');
+addBookToLibrary('Open veins of South America', 'Some guy', '456 pages');
+addBookToLibrary('Open veins of South America', 'Some guy', '456 pages');
+addBookToLibrary('PRisoners of Georgrpahy', 'Some guy', '456 pages');
+addBookToLibrary('Open veins of South America', 'Some guy', '456 pages');
+addBookToLibrary('Open veins of South America', 'Some guy', '456 pages');
+addBookToLibrary('Open veins of South America', 'Some guy', '456 pages');
+addBookToLibrary('Oh dearrrrrr', 'Some guy', '456 pages');
+addBookToLibrary('58573739', 'Some guy', '456 pages');
+addBookToLibrary('48 laws of power', 'Some guy', '456 pages');
+addBookToLibrary('The subtle art of not', 'Some guy', '456 pages');
+addBookToLibrary('How to run away from People', 'Some guy', '456 pages');
+render();
 
-const parent = document.getElementById("container")
-    parent.addEventListener("click", event => {
-        if (event.target.className === "removeButton") {
-            removeBook(event.target.parentNode.dataset.index)
-        }
-
-        else if (event.target.className === "readToggle") {
-            toggleRead(event.target.parentNode.dataset.index, event.target)
-        }
-})
+const parent = document.getElementById('container');
+parent.addEventListener('click', (event) => {
+  if (event.target.className === 'removeButton') {
+    removeBook(event.target.parentNode.dataset.index);
+  } else if (event.target.className === 'readToggle') {
+    toggleRead(event.target.parentNode.dataset.index, event.target);
+  }
+});
--- tictactoe/tictac.js
+++ tictactoe/tictac.js
@@ -1,71 +1,59 @@
-const container = document.querySelector("#container");
+const container = document.querySelector('#container');
 
-let GameBoard = {
-    grid: ["", "", "", "", "", "", "", "", ""],
-    turn: "X"
+const GameBoard = {
+  grid: ['', '', '', '', '', '', '', '', ''],
+  turn: 'X',
 };
 
-function Player(name, symbol){
-    this.name = name;
-    this.symbol = symbol;
-};
-
-function render(){
-    function removeAllChildNodes(container) {
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
-    }
-    removeAllChildNodes(container)
-    let i = 0;
-    GameBoard.grid.forEach(square => {
-        const div = document.createElement("div")
-        div.className = "symbols";
-        div.id = i;
-        div.textContent = square;
-        container.appendChild(div)
-        i ++
-    }
-        
-)}
-
-function Game(event){
-    if (GameBoard.grid[event.id] == ""){
-    GameBoard.grid[event.id] = GameBoard.turn
-    event.textContent = GameBoard.turn};
-    if (GameBoard.turn == "X"){
-        GameBoard.turn = "O"}
-        else {GameBoard.turn = "X"}
-        //checking X rows
-    if (GameBoard.grid[0] == "X" && GameBoard.grid[1] == "X" && GameBoard.grid[2] == "X"){alert("Win")}
-    else if (GameBoard.grid[3] == "X" && GameBoard.grid[4] == "X" && GameBoard.grid[5] == "X"){alert("Win")}
-    else if (GameBoard.grid[6] == "X" && GameBoard.grid[7] == "X" && GameBoard.grid[8] == "X"){alert("Win")}
-        //checking X columns
-    else if (GameBoard.grid[0] == "X" && GameBoard.grid[3] == "X" && GameBoard.grid[6] == "X"){alert("Win")}
-    else if (GameBoard.grid[1] == "X" && GameBoard.grid[4] == "X" && GameBoard.grid[7] == "X"){alert("Win")}
-    else if (GameBoard.grid[2] == "X" && GameBoard.grid[5] == "X" && GameBoard.grid[8] == "X"){alert("Win")}
-        //checking O rows
-    if (GameBoard.grid[0] == "O" && GameBoard.grid[1] == "O" && GameBoard.grid[2] == "O"){alert("O Win")}
-    else if (GameBoard.grid[3] == "O" && GameBoard.grid[4] == "O" && GameBoard.grid[5] == "O"){alert("O Win")}
-    else if (GameBoard.grid[6] == "O" && GameBoard.grid[7] == "O" && GameBoard.grid[8] == "O"){alert("O Win")}
-        //checking O columns
-    else if (GameBoard.grid[0] == "O" && GameBoard.grid[3] == "O" && GameBoard.grid[6] == "O"){alert("O Win")}
-    else if (GameBoard.grid[1] == "O" && GameBoard.grid[4] == "O" && GameBoard.grid[7] == "O"){alert("O Win")}
-    else if (GameBoard.grid[2] == "O" && GameBoard.grid[5] == "O" && GameBoard.grid[8] == "O"){alert("O Win")}
-        //checking diag
-    else if (GameBoard.grid[0] == "X" && GameBoard.grid[4] == "X" && GameBoard.grid[8] == "X"){alert(" Win")}
-    else if (GameBoard.grid[0] == "O" && GameBoard.grid[4] == "O" && GameBoard.grid[8] == "O"){alert("O Win")}
-    else if (GameBoard.grid[2] == "X" && GameBoard.grid[4] == "X" && GameBoard.grid[6] == "X"){alert(" Win")}
-    else if (GameBoard.grid[2] == "O" && GameBoard.grid[4] == "O" && GameBoard.grid[6] == "O"){alert("O Win")}
-    else if (!GameBoard.grid.includes("")) {alert("Draw!")}
+function Player(name, symbol) {
+  this.name = name;
+  this.symbol = symbol;
 }
 
-const playerOne = new Player("Sam", "X");
-const playerTwo = new Player("Billy", "O")
+function render() {
+  function removeAllChildNodes(container) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+  removeAllChildNodes(container);
+  let i = 0;
+  GameBoard.grid.forEach((square) => {
+    const div = document.createElement('div');
+    div.className = 'symbols';
+    div.id = i;
+    div.textContent = square;
+    container.appendChild(div);
+    i++;
+  });
+}
+
+function Game(event) {
+  if (GameBoard.grid[event.id] == '') {
+    GameBoard.grid[event.id] = GameBoard.turn;
+    event.textContent = GameBoard.turn;
+  }
+  if (GameBoard.turn == 'X') {
+    GameBoard.turn = 'O';
+  } else { GameBoard.turn = 'X'; }
+  // checking X rows
+  if (GameBoard.grid[0] == 'X' && GameBoard.grid[1] == 'X' && GameBoard.grid[2] == 'X') { alert('Win'); } else if (GameBoard.grid[3] == 'X' && GameBoard.grid[4] == 'X' && GameBoard.grid[5] == 'X') { alert('Win'); } else if (GameBoard.grid[6] == 'X' && GameBoard.grid[7] == 'X' && GameBoard.grid[8] == 'X') { alert('Win'); }
+  // checking X columns
+  else if (GameBoard.grid[0] == 'X' && GameBoard.grid[3] == 'X' && GameBoard.grid[6] == 'X') { alert('Win'); } else if (GameBoard.grid[1] == 'X' && GameBoard.grid[4] == 'X' && GameBoard.grid[7] == 'X') { alert('Win'); } else if (GameBoard.grid[2] == 'X' && GameBoard.grid[5] == 'X' && GameBoard.grid[8] == 'X') { alert('Win'); }
+  // checking O rows
+  if (GameBoard.grid[0] == 'O' && GameBoard.grid[1] == 'O' && GameBoard.grid[2] == 'O') { alert('O Win'); } else if (GameBoard.grid[3] == 'O' && GameBoard.grid[4] == 'O' && GameBoard.grid[5] == 'O') { alert('O Win'); } else if (GameBoard.grid[6] == 'O' && GameBoard.grid[7] == 'O' && GameBoard.grid[8] == 'O') { alert('O Win'); }
+  // checking O columns
+  else if (GameBoard.grid[0] == 'O' && GameBoard.grid[3] == 'O' && GameBoard.grid[6] == 'O') { alert('O Win'); } else if (GameBoard.grid[1] == 'O' && GameBoard.grid[4] == 'O' && GameBoard.grid[7] == 'O') { alert('O Win'); } else if (GameBoard.grid[2] == 'O' && GameBoard.grid[5] == 'O' && GameBoard.grid[8] == 'O') { alert('O Win'); }
+  // checking diag
+  else if (GameBoard.grid[0] == 'X' && GameBoard.grid[4] == 'X' && GameBoard.grid[8] == 'X') { alert(' Win'); } else if (GameBoard.grid[0] == 'O' && GameBoard.grid[4] == 'O' && GameBoard.grid[8] == 'O') { alert('O Win'); } else if (GameBoard.grid[2] == 'X' && GameBoard.grid[4] == 'X' && GameBoard.grid[6] == 'X') { alert(' Win'); } else if (GameBoard.grid[2] == 'O' && GameBoard.grid[4] == 'O' && GameBoard.grid[6] == 'O') { alert('O Win'); } else if (!GameBoard.grid.includes('')) { alert('Draw!'); }
+}
+
+const playerOne = new Player('Sam', 'X');
+const playerTwo = new Player('Billy', 'O');
 render();
 
-document.querySelectorAll(".symbols").forEach( symbol => {
-    symbol.addEventListener("click", event => {
-        Game(event.target)
-    })
-}) 
+document.querySelectorAll('.symbols').forEach((symbol) => {
+  symbol.addEventListener('click', (event) => {
+    Game(event.target);
+  });
+});
