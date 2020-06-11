7dcb969d70f5130a2b60e2a20c407e2971486d62
aslan <social.smur@simplelogin.fr>
2020-06-11 19:22:59 +0100

    Updated to use firestore

--- library/book.js
+++ library/book.js
@@ -1,36 +1,35 @@
 const container = document.querySelector('#container');
-const myLibrary = [];
 
-function render() {
+function render(doc) {
   const div = document.createElement('div');
   const button = document.createElement('button');
   const read = document.createElement('button');
-  myLibrary.forEach(item => {
-    div.className = 'books';
-    button.className = 'removeButton';
-    button.textContent = 'Remove';
-    read.className = 'readToggle';
-    if (item.read === 'Not read') {
-      read.textContent = 'Read?';
-    } else {
-      read.textContent = 'Read';
-    }
-    div.dataset.index = myLibrary.length - 1;
-    div.innerHTML = `<h1>${item.title}<h1> 
-            <h2>${item.author}<h2>
-            <h3>${item.pages} pages<h3>`;
-    container.appendChild(div);
-    div.appendChild(button);
-    div.appendChild(read);
-  });
-}
+  div.className = 'books';
+  button.className = 'removeButton';
+  button.textContent = 'Remove';
+  read.className = 'readToggle';
+  if (doc.data().read === 'Not read') {
+    read.textContent = 'Read?';
+  } else {
+    read.textContent = 'Read';
+  }
+  let attr = 'A' + doc.id
+  div.setAttribute('data-id', attr);
+  div.innerHTML = `<h1>${doc.data().title}<h1> 
+          <h2>${doc.data().author}<h2>
+          <h3>${doc.data().pages} pages<h3>`;
+  container.appendChild(div);
+  div.appendChild(button);
+  div.appendChild(read);
+};
 
-Object.keys(localStorage).forEach(key => {
+// if using localStorage
+/* Object.keys(localStorage).forEach(key => {
   const item = localStorage.getItem(key);
   const itemParsed = JSON.parse(item);
   myLibrary.push(itemParsed);
   render();
-});
+}); */
 
 class Book {
   constructor(title, author, pages, read) {
@@ -41,7 +40,8 @@ class Book {
   }
 }
 
-function addBookToLibrary(title, author, pages, read) {
+// below is using localStorage
+/* function addBookToLibrary(title, author, pages, read) {
   // creates object from inputted data and appends it to library array
   const bookObj = new Book(title, author, pages, read);
   localStorage.setItem(JSON.stringify(bookObj.title), JSON.stringify(bookObj));
@@ -52,31 +52,36 @@ function addBookToLibrary(title, author, pages, read) {
   const localBook = JSON.parse(book);
   myLibrary.push(localBook);
   render();
+} */
+
+function addBookToLibrary(title, author, pages, read) {
+  // creates object from inputted data and appends it to library array
+  db.collection('books').add({
+    title,
+    author,
+    pages,
+    read
+  });
 }
 
-function toggleRead(index, e) {
-  if (myLibrary[index].read === 'Not read') {
-    myLibrary[index].read = 'Read';
-    e.innerHTML = 'Read';
-  } else {
-    myLibrary[index].read = 'Not read';
-    e.innerHTML = 'Read?';
-  }
-}
+// need to update this to correctly update on firestore
+function toggleRead(id, e) {
+  db.collection('books').doc(id).update({
+    read: 'Read'
+  });
+};
+
 
 function removeBook(index) {
-  const bookTitle = index.parentNode.firstChild.textContent;
-  localStorage.removeItem(`"${bookTitle}"`);
-  myLibrary.splice(index.parentNode.dataset.index, 1);
+  let tempid = index.parentNode.dataset.id;
+  let id = tempid.slice(1);
+  db.collection('books')
+    .doc(id)
+    .delete();
   const div = Array.from(document.getElementsByClassName('books'));
   div.forEach(item => {
-    if (item.dataset.index === index.parentNode.dataset.index) {
-      container.removeChild(item);
-    }
-  });
-  div.forEach(item => {
     const itemObj = item;
-    if (item.dataset.index > index.parentNode.dataset.index) {
+    if (item.dataset.index > index) {
       itemObj.dataset.index -= 1;
     }
   });
@@ -128,6 +133,19 @@ parent.addEventListener('click', event => {
   if (event.target.className === 'removeButton') {
     removeBook(event.target);
   } else if (event.target.className === 'readToggle') {
-    toggleRead(event.target.parentNode.dataset.index, event.target);
+    toggleRead(event.target.parentNode.dataset.id, event.target);
   }
+});
+
+// real time listener
+db.collection('books').onSnapshot(snapshot => {
+  const changes = snapshot.docChanges();
+  changes.forEach(change => {
+    if (change.type === 'added') {
+      render(change.doc);
+    } else if (change.type === 'removed') {
+      const li = container.querySelector(`[data-id=A${change.doc.id}]`);
+      container.removeChild(li);
+    }
+  });
 });
