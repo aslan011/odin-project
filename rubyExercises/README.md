e1bc7ceca1ca2d35282cf36fe11a93f8e1974509
aslan <social.smur@simplelogin.fr>
2020-06-26 20:01:21 +0100

    Removed ruby exercises

--- rubyExercises/README.md
+++
@@ -1,7 +0,0 @@
-## Description
-
-Initally began to look at Ruby and Ruby on Rails, however I decided to stick with Javascript for now, given the potential for NodeJS.
-
-## Future
-
-*May look into furthering my skills within this, if required.
--- rubyExercises/substrings.rb
+++
@@ -1,9 +0,0 @@
-dict = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-def substrings (array)
-    newArray = array.split(" ")
-    
-    new = newArray.downcase.gsub(/\W+/, ' ')
-    puts new
-end
-substrings("Hello, what is the limit?!")
